f420d222eb02e5d85f9d023a3680df723f6b7005
Caleb Ogbebor <calebrandy@gmail.com>
2024-03-01 17:39:40 +0100

    testing git branches

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@
 export default function Index() {
   return (
     <>
-    <p>hello</p>
-    <p>world</p>
+      <main>
+        <div>
+          <h1>Learning about GIT branches</h1>
+        </div>
+        <p>lorem njfnnniwij niurgijoheroihjiujhkiojhihmjh jbhjrbgughuhijimhoh09jh45h kjihiojhihijhithuihhjh ijih5uhi4hi4hhj</p>
+      </main>
     </>
   );
 }
